039c3b01f4ce95061e3834c70315ae58456ade64
Just Ine <87438032+just337ine@users.noreply.github.com>
2024-01-30 19:06:52 +0100

    No operation function

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/*
+ * nop - nop doesn't do anything
+ * @stack: pointer to the stack
+ * @line_number: line number where the opcode occurs
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
