d5faf191393393c5e927796afa00c34b3430cd45
Just Ine <87438032+just337ine@users.noreply.github.com>
2024-01-30 19:22:26 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -110,4 +110,8 @@ void nop(stack_t **stack, unsigned int line_number);
 void pchar(stack_t **stack, unsigned int line_number);
 void pstr(stack_t **stack, unsigned int line_number);
 
+/*** rotate.c file ***/
+void rotl(stack_t **stack, unsigned int line_number);
+void rotr(stack_t **stack, unsigned int line_number);
+
 #endif /* MONTY_H */
