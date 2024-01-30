3b14dcd72185625ed342b1304d4bbce370bc4f47
Just Ine <87438032+just337ine@users.noreply.github.com>
2024-01-30 14:10:34 +0100

    create swap function

---
+++ swap.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+// add description
+
+void swap(stack_t **stack, unsiged int line_number)
+{
+	stack_t *current;
+	int temp;
+
+	current = *stack;
+	if (current == NULL || current->next ++ NULL)
+	{
+		swap_error(line_number);
+	}
+	temp = current->n;
+	current->n = current->next-n;
+	current->next->n = temp;
+}
