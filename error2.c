aaa6b7489151946fbb02e08bd02697a48db56860
Just Ine <87438032+just337ine@users.noreply.github.com>
2024-01-30 14:15:08 +0100

    update file

--- error2.c
+++ error2.c
@@ -6,3 +6,15 @@ void unknown_error(unsigned int c, char *line)
 	fprintf(stderr, "L%d: unknown instruction %s\n", c, line);
 	exit(EXIT_FAILURE);
 }
+
+void swap_error(unsigned int line_number)
+{
+	fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+	exit(EXIT_FAILURE);
+}
+
+void add_error(unsigned int line_number)
+{
+	fprintf(stderr, "L%d: cain't add, stack too short\n", line_number);
+	exit(EXIT_FAILURE);
+}
--- swap.c
+++ swap.c
@@ -1,17 +1,17 @@
 #include "monty.h"
 // add description
 
-void swap(stack_t **stack, unsiged int line_number)
+void swap(stack_t **stack, unsigned int line_number)
 {
 	stack_t *current;
 	int temp;
 
 	current = *stack;
-	if (current == NULL || current->next ++ NULL)
+	if (current == NULL || current->next == NULL)
 	{
 		swap_error(line_number);
 	}
 	temp = current->n;
-	current->n = current->next-n;
+	current->n = current->next->n;
 	current->next->n = temp;
 }
