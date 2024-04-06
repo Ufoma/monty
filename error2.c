107382dcdc136522c78861cb79cbfc7e02062308
Ufoma <dymondintheski@gmail.com>
2024-04-06 00:33:16 -0700

    fixed error2 file

--- error2.c
+++ error2.c
@@ -1,5 +1,9 @@
 #include "monty.h"
-// add descrioption
+/**
+ * unknown_error - Handles unknown error
+ * @c: Line number where the error occured
+ * @line: The unknown instruction
+ */
 
 void unknown_error(unsigned int c, char *line)
 {
@@ -7,11 +11,21 @@ void unknown_error(unsigned int c, char *line)
 	exit(EXIT_FAILURE);
 }
 
+/**
+ * swap_error - Handles stack too short error for swap instruction.
+ * @line_number: The line number where the error occurred.
+ */
+
 void swap_error(unsigned int line_number)
 {
 	fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 	exit(EXIT_FAILURE);
 }
+
+/**
+ * add_error - handles error of add instruction
+ * @line_number: where the instruction is to be executed
+ */
 
 void add_error(unsigned int line_number)
 {
